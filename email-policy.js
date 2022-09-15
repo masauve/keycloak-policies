220d10b3f37f5b058c5de2774bbfdb3b00d9d8b0
masauve <msauve@redhat.com>
2022-09-15 13:58:21 -0400

    fixed js

--- email-policy.js
+++ email-policy.js
@@ -1,7 +1,7 @@
-const context = $evaluation.getContext();
-const identity = context.getIdentity();
-const attributes = identity.getAttributes();
-const email = attributes.getValue('email').asString(0);
+var context = $evaluation.getContext();
+var identity = context.getIdentity();
+var attributes = identity.getAttributes();
+var email = attributes.getValue('email').asString(0);
 
 if (email.endsWith('@redhat.com')) {
     $evaluation.grant();
